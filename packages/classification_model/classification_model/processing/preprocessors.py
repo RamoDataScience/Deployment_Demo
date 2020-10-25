c03e1a428428a2394358b8be22821cab1ddf1ab7
RamoDataScience <azzouz.omar.zayen@gmail.com>
2020-10-25 16:07:57 -0400

    My features

--- packages/classification_model/classification_model/processing/preprocessors.py
+++ packages/classification_model/classification_model/processing/preprocessors.py
@@ -126,32 +126,3 @@ class CategoricalEncoder(BaseEstimator, TransformerMixin):
             )
         return X
 
-
-# logarithm transformer
-class LogTransformer(BaseEstimator, TransformerMixin):
-
-    def __init__(self, variables = None) -> None:
-        if not isinstance(variables, list):
-            self.variables = [variables]
-        else:
-            self.variables = variables
-
-    def fit(self, X: pd.DataFrame, y: pd.Series = None) -> "LogTransformer":
-        # to accomodate the pipeline
-        return self
-
-    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        X = X.copy()
-
-        # check that the values are non-negative for log transform
-        if not (X[self.variables] > 0).all().all():
-            vars_ = self.variables[(X[self.variables] <= 0).any()]
-            raise ValueError(
-                f"Variables contain zero or negative values, "
-                f"can't apply log for vars: {vars_}"
-            )
-
-        for feature in self.variables:
-            X[feature] = np.log(X[feature])
-
-        return X
