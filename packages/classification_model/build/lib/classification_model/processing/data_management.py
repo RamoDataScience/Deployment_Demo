0fe86f14317816f4e4ecc1152a5e0659e480ab27
RamoDataScience <azzouz.omar.zayen@gmail.com>
2020-10-31 19:58:00 -0400

    s3 test v54

--- packages/classification_model/build/lib/classification_model/processing/data_management.py
+++ packages/classification_model/build/lib/classification_model/processing/data_management.py
@@ -35,7 +35,7 @@ def save_pipeline(*, pipeline_to_persist) -> None:
     save_file_name = f"{config.PIPELINE_SAVE_FILE}{_version}.pkl"
     save_path = config.TRAINED_MODEL_DIR / save_file_name
 
-    remove_old_pipelines(files_to_keep=[save_file_name])
+    remove_old_pipelines(files_to_keep=save_file_name)
     joblib.dump(pipeline_to_persist, save_path)
     _logger.info(f"saved pipeline: {save_file_name}")
 
@@ -58,7 +58,7 @@ def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
     However, we do also include the immediate previous
     pipeline version for differential testing purposes.
     """
-    do_not_delete = files_to_keep + ['__init__.py']
+    do_not_delete = [files_to_keep] + ['__init__.py']
     for model_file in config.TRAINED_MODEL_DIR.iterdir():
         if model_file.name not in do_not_delete:
             model_file.unlink()
